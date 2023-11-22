62bc1fbce0d38e9627250e86d365bd67c4ea1efc
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-11-21 20:28:53 -0800

    added all translations to staking page

--- src/Components/Stacking/Hero/RewardTable.tsx
+++ src/Components/Stacking/Hero/RewardTable.tsx
@@ -6,19 +6,19 @@ const RewardTable = () => {
   return (
     <div className="w-full lg:h-[656px] flex flex-col items-center justify-start gap-4 bg-black-main rounded-xl p-7 lg:mt-20">
       <div className="w-full flex flex-col items-center justify-center gap-4 border border-white-main rounded-[10px] p-6">
-        <p className="text-white-main text-[26px] sm:text-[32px] font-bold">
+        <p className="text-white-main text-[26px] sm:text-[32px] font-bold text-center">
           {t("Staked")}
         </p>
         <p className="text-white-main text-lg sm:text-xl font-medium">0.0000</p>
       </div>
       <div className="w-full flex flex-col items-center justify-center gap-4 border border-white-main rounded-[10px] p-6">
-        <p className="text-white-main text-[26px] sm:text-[32px] font-bold">
+        <p className="text-white-main text-[26px] sm:text-[32px] font-bold text-center">
           {t("Unstaked")}
         </p>
         <p className="text-white-main text-lg sm:text-xl font-medium">0.0000</p>
       </div>
       <div className="w-full flex flex-col items-center justify-center gap-4 border border-white-main rounded-[10px] p-6">
-        <p className="text-white-main text-[26px] sm:text-[32px] font-bold">
+        <p className="text-white-main text-[26px] sm:text-[32px] font-bold text-center">
           {t("Rewards")}
         </p>
         <p className="text-white-main text-lg sm:text-xl font-medium">0.0000</p>
